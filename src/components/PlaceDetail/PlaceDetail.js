158be4f516342b11b464accee58a29a1cd251833
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2019-06-30 16:24:07 +1200

    react-native-vector-icons added

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,6 +1,16 @@
 import React from "react";
 
-import { Modal, Image, Text, View, Button, StyleSheet } from "react-native";
+import {
+  Modal,
+  Image,
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+
+import Icon from "react-native-vector-icons/Ionicons";
 
 const placeDetail = props => {
   let modalContent = null;
@@ -22,7 +32,11 @@ const placeDetail = props => {
       <View style={styles.modalContainer}>
         {modalContent}
         <View>
-          <Button title="Delete" color="red" onPress={props.onItemDeleted} />
+          <TouchableOpacity onPress={props.onItemDeleted}>
+            <View style={styles.deleteButton}>
+              <Icon size={30} name="ios-trash" color="red" />
+            </View>
+          </TouchableOpacity>
           <Button title="Close" onPress={props.onModalClosed} />
         </View>
       </View>
@@ -40,6 +54,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 28
+  },
+  deleteButton: {
+    alignItems: "center"
   }
 });
 
