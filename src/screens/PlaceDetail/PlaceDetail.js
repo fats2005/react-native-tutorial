4dcbb952b4421dea80660f3ca8822101e9439427
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2019-07-02 15:45:20 +1200

    pushing pages

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import {
-  Modal,
   Image,
   Text,
   View,
@@ -13,39 +12,25 @@ import {
 import Icon from "react-native-vector-icons/Ionicons";
 
 const placeDetail = props => {
-  let modalContent = null;
-
-  if (props.selectedPlace) {
-    modalContent = (
+  return (
+    <View style={styles.containar}>
       <View>
         <Image source={props.selectedPlace.image} style={styles.placeImage} />
         <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
       </View>
-    );
-  }
-  return (
-    <Modal
-      onRequestClose={props.onModalClosed}
-      visible={props.selectedPlace !== null}
-      animationType="slide"
-    >
-      <View style={styles.modalContainer}>
-        {modalContent}
-        <View>
-          <TouchableOpacity onPress={props.onItemDeleted}>
-            <View style={styles.deleteButton}>
-              <Icon size={30} name="ios-trash" color="red" />
-            </View>
-          </TouchableOpacity>
-          <Button title="Close" onPress={props.onModalClosed} />
-        </View>
+      <View>
+        <TouchableOpacity onPress={props.onItemDeleted}>
+          <View style={styles.deleteButton}>
+            <Icon size={30} name="ios-trash" color="red" />
+          </View>
+        </TouchableOpacity>
       </View>
-    </Modal>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  modalContainer: { margin: 22 },
+  containar: { margin: 22 },
   placeImage: {
     width: "100%",
     height: 200
