987de62ff12f145d2314adda0f47a0b4e19f61cc
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2019-07-09 18:12:39 +1200

    add react-native-maps

--- android/app/src/main/java/com/rncourse/MainApplication.java
+++ android/app/src/main/java/com/rncourse/MainApplication.java
@@ -3,6 +3,7 @@ package com.rncourse;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -58,6 +59,7 @@ public class MainApplication extends NavigationApplication {
 		return Arrays.<ReactPackage>asList(
 			// eg. new VectorIconsPackage()
       new VectorIconsPackage()
+      new MapsPackage()
 		);
 	}
 
